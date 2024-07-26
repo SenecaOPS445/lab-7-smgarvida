a53e347d64cfb8377a2bdb7a52f860ba04f10725
Shayna Mae Garvida <smgarvida@myseneca.ca>
2024-07-26 14:58:27 -0400

    Completed lab7

--- lab7a.py
+++ lab7a.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: smgarvida
+
 class Time:
-    """Simple object type for time of the day.
-    data attributes: hour, minute, second
-    """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+   """Simple object type for time of the day.
+      data attributes: hour, minute, second
+   """
+   def __init__(self,hour=12,minute=0,second=0):
+       """constructor for time object""" 
+       self.hour = hour
+       self.minute = minute
+       self.second = second
 
 def format_time(t):
     """Return time object (t) as a formatted string"""
-    return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
+    return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}' % (t.hour, t.minute, t.second)
 
 def sum_times(t1, t2):
     """Add two time objests and return the sum."""
@@ -24,9 +25,9 @@ def sum_times(t1, t2):
 
 def valid_time(t):
     """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+       24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
     if t.hour < 0 or t.minute < 0 or t.second < 0:
-        return False
+       return False
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
-        return False
+       return False
     return True
